756def1b2cdda4166fc8c98bfd8184c1ca9bbe56
Peter Harrison <peter.harrison@helicron.net>
2022-09-10 22:21:35 +0100

    sync up the config files

--- mazerunner-core/config-dorothy.h
+++ mazerunner-core/config-dorothy.h
@@ -1,14 +1,14 @@
 #ifndef DOROTHY_H
 #define DOROTHY_H
 
-// time between logged lined when reporting is enabled (milliseconds)
-const int REPORTING_INTERVAL = 10;
-
 const uint32_t BAUDRATE = 115200;
 const int SENSOR_COUNT = 4;
 const float MAX_MOTOR_VOLTS = 6.0;
 
 //***************************************************************************//
+
+// time between logged lined when reporting is enabled (milliseconds)
+const int REPORTING_INTERVAL = 10;
 
 const float WHEEL_DIAMETER = 32.0;
 const float ENCODER_PULSES = 12.0;
@@ -150,12 +150,14 @@ const int FRONT_REFERENCE = 850; // reading when mouse centered with wall ahead
 const float left_edge_pos = 90.0f;
 const float right_edge_pos = 93.0f;
 
+// These take no storage - the compiler uses the values directly
 const TurnParameters turn_params[4] = {
     {DEFAULT_TURN_SPEED, 25, 10, -90, 280, 4000, TURN_THRESHOLD_SS90E}, // 0 => SS90EL
     {DEFAULT_TURN_SPEED, 20, 10, 90, 280, 4000, TURN_THRESHOLD_SS90E},  // 0 => SS90ER
     {DEFAULT_TURN_SPEED, 20, 10, -90, 280, 4000, TURN_THRESHOLD_SS90E}, // 0 => SS90L
     {DEFAULT_TURN_SPEED, 20, 10, 90, 280, 4000, TURN_THRESHOLD_SS90E},  // 0 => SS90R
 };
+
 //***************************************************************************//
 //***************************************************************************//
 // Some physical constants that are likely to be board -specific
