c8c29f092dcb081afcc32ca04850effb40b4442a
Peter Harrison <peter.harrison@helicron.net>
2022-10-31 22:13:20 +0000

    simplify and annotate the null serial class

--- mazerunner-core/src/serial_null.h
+++ mazerunner-core/src/serial_null.h
@@ -4,7 +4,7 @@
  * File Created: Monday, 31st October 2022 12:46:17 pm                        *
  * Author: Peter Harrison                                                     *
  * -----                                                                      *
- * Last Modified: Monday, 31st October 2022 12:46:49 pm                       *
+ * Last Modified: Monday, 31st October 2022 10:13:20 pm                       * 
  * -----                                                                      *
  * Copyright 2022 - 2022 Peter Harrison, Micromouseonline                     *
  * -----                                                                      *
@@ -23,35 +23,47 @@ public:
   // public methods
   NullSerial() {}
   ~NullSerial() {}
+
   void begin(long speed) {
     (void)speed;
   }
-  bool listen() {
-    return true;
-  }
-  void end() {}
-  bool isListening() {
-    return true;
-  }
-  bool overflow() {
-    return false;
-  }
-  int peek() {
+
+  // These need to be implemented for a concrete
+  // class because Stream is pure virtual
+
+  /***
+   * Disables serial communication, allowing the RX and TX pins to be
+   * used for general input and output.
+   * To re-enable serial communication, call Serial.begin().
+   */
+  virtual void end() {}
+
+  /***
+   * Returns the next byte (character) of incoming serial data without
+   * removing it from the internal serial buffer. That is, successive
+   * calls to peek() will return the same character, as will the next
+   * call to read().
+   * Serial.peek() inherits from the Stream utility class.
+   *
+   * RETURN: The first byte of incoming serial data available
+   *         (or -1 if no data is available). Data type: int.
+   */
+  virtual int peek() {
     return -1;
   }
 
+  /***
+   * Writes binary data to the serial port. This data is sent as a byte
+   * or series of bytes; to send the characters representing the digits
+   * of a number use the print() function instead.
+   *
+   * RETURN: will return the number of bytes written, though reading that
+   *         number is optional. Data type: size_t.
+   */
   virtual size_t write(unsigned char byte) {
     (void)byte;
     return 0;
   }
-  virtual int read() {
-    return -1;
-  }
-  virtual int available() {
-    return 0;
-  }
-  virtual void flush() {}
-
   size_t write(const char *str) {
     (void)str;
     return 0;
@@ -62,79 +74,40 @@ public:
     return 0;
   }
 
-  size_t print(const __FlashStringHelper *) {
-    return 0;
+  /***
+   * Reads incoming serial data - probably from a buffer
+   *
+   * RETURN: first byte of incoming data or -1 if none is available
+   *         (EOF is probably defined as -1 in many implementations)
+   */
+  virtual int read() {
+    return -1;
   }
-  size_t print(const String &) {
-    return 0;
-  }
-  size_t print(const char[]) {
-    return 0;
-  }
-  size_t print(char) {
-    return 0;
-  }
-  size_t print(unsigned char, int = DEC) {
-    return 0;
-  }
-  size_t print(int, int = DEC) {
-    return 0;
-  }
-  size_t print(unsigned int, int = DEC) {
-    return 0;
-  }
-  size_t print(long, int = DEC) {
-    return 0;
-  }
-  size_t print(unsigned long, int = DEC) {
-    return 0;
-  }
-  size_t print(double, int = 2) {
-    return 0;
-  }
-  size_t print(const Printable &) {
+
+  /***
+   * Get the number of bytes (characters) available for reading from
+   * the serial port. This is data that’s already arrived and stored
+   * in the serial receive buffer (which holds 64 bytes).
+   *
+   * Serial.available() inherits from the Stream utility class.
+   *
+   * RETURN: the number of bytes in the buffer.
+   */
+  virtual int available() {
     return 0;
   }
 
-  size_t println(const __FlashStringHelper *f) {
-    (void)f;
-    return 0;
-  }
-  size_t println(const String &s) {
-    (void)s;
-    return 0;
-  }
-  size_t println(const char[]) {
-    return 0;
-  }
-  size_t println(char) {
-    return 0;
-  }
-  size_t println(unsigned char, int = DEC) {
-    return 0;
-  }
-  size_t println(int, int = DEC) {
-    return 0;
-  }
-  size_t println(unsigned int, int = DEC) {
-    return 0;
-  }
-  size_t println(long, int = DEC) {
-    return 0;
-  }
-  size_t println(unsigned long, int = DEC) {
-    return 0;
-  }
-  size_t println(double, int = 2) {
-    return 0;
-  }
-  size_t println(const Printable &) {
-    return 0;
-  }
-  size_t println(void) {
-    return 0;
-  }
+  /***
+   * Waits for the transmission of outgoing serial data to complete.
+   * (Prior to Arduino 1.0, this instead removed any buffered
+   * incoming serial data.)
+   *
+   * flush() inherits from the Stream utility class.
+   *
+   * RETURN: nothing
+   */
+  virtual void flush() {}
 
   // public only for easy access by interrupt handlers
-  static inline void handle_interrupt();
+  void handle_interrupt();
 };
