88e5117e602d9c5f548d6b7c2049cbe45fe42b2e
Peter Harrison <peter.harrison@helicron.net>
2023-07-08 15:40:39 +0100

    switch to HOME calibration

--- mazerunner-core/config.h
+++ mazerunner-core/config.h
@@ -82,7 +82,7 @@ struct TurnParameters {
 #define EVENT_APEC 4
 
 // choose the one you will be using BEFORE selecting the robot below
-#define EVENT EVENT_UK
+#define EVENT EVENT_HOME
 
 /*************************************************************************/
 /***
