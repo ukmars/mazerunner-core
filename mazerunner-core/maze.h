ff2eb7b4333a7b5406d89e2cc0962f0449188aea
Peter Harrison <peter.harrison@helicron.net>
2023-08-01 10:06:00 +0100

    some more comments in maze.h

--- mazerunner-core/maze.h
+++ mazerunner-core/maze.h
@@ -116,15 +116,50 @@ inline Heading behind_from(const Heading heading) {
 }
 //***************************************************************************//
 
+/***
+ * Directions are relative to the robot's orientation. They do not refer to
+ * any particular heading and are just used to make code more readable where
+ * decisions are made about which way to turn.
+ */
+
 enum Direction { AHEAD, RIGHT, BACK, LEFT, DIRECTION_COUNT };
+
 //***************************************************************************//
 
-#define MAX_COST 255
+/***
+ * For the classic micromouse contest, the maze is 16x16 cells. That is
+ * convenient because a single byte can be used to store the cost associated
+ * with each cell when performing a simple flood.
+ *
+ * If you extend the code to cope with a half-size maze of up to 32x32 cells
+ * you will need to make changes to the way the cost is stored.
+ *
+ * A more sophisticated method for flooding the maze might also require larger
+ * values for cost and changes will be needed. the AVR has very limited RAM
+ * so the simplest option is used here.
+ */
 #define MAZE_WIDTH 16
 #define MAZE_CELL_COUNT (MAZE_WIDTH * MAZE_WIDTH)
+#define MAX_COST (MAZE_CELL_COUNT - 1)
 
 //***************************************************************************//
 
+/***
+ * Location stores a position in the maze as an (x,y) coordinate pair. This is
+ * probably a more intuitive representation than storing just a single index
+ * into and array. The method takes a little more code to manage than historical
+ * methods but they were devised for processors with very little program memory.
+ *
+ * This is a example of more processor power making is easier to represent data
+ * in a more user-friendly manner.
+ *
+ * Locations have a number of supporting operations collected into the struct so
+ * that you don't have to keep re-writing the same bits of code.
+ *
+ * Note that in C++ a struct is functionally identical to a class but with all
+ * members public by default.
+ *
+ */
 struct Location {
   uint8_t x;
   uint8_t y;
@@ -184,23 +219,48 @@ struct Location {
 
 //***************************************************************************//
 
+/***
+ * The Maze class is the heart of the micromouse data.
+ *
+ * In this basic version, the maze has a single goal location even though all
+ * micromouse mazes can have a rectangular region that defines a goal area.
+ *
+ * The two main data blocks in the class store the wall state of every cell
+ * and a cost associated with every cell after the maze is flooded.
+ *
+ * To keep code simple, you will note that each wall is stored twice - once
+ * as seen from each side. The methods in the maze take care to ensure that
+ * changes to a wall are recorded in both associated cells.
+ *
+ * Before flooding the maze, take care to set the maze mask as appropriate.
+ * See the description above for details of the mask.
+ *
+ * When the robot searches and updates the map, you should call the method
+ * update_wall_state() to record changes. The similar-looking method
+ * set_wall_state() is private for a reason. It is unconditional and may
+ * result in walls being changed after they were frst seen.
+ *
+ */
 class Maze {
  public:
   Maze() {
   }
 
   Location goal() const {
-    return m_goal_loc;
+    return m_goal;
   }
 
+  /// @brief  changes the default goal. For example in a practice maze
   void set_goal(const Location goal) {
-    m_goal_loc = goal;
+    m_goal = goal;
   }
 
-  WallInfo walls(const Location loc) const {
-    return m_walls[loc.x][loc.y];
+  /// @brief  return the state of the walls in a cell
+  WallInfo walls(const Location cell) const {
+    return m_walls[cell.x][cell.y];
   }
 
+  /// @brief return true if ANY walls in a cell have NOT been seen
   bool has_unknown_walls(const Location cell) const {
     WallInfo walls_here = m_walls[cell.x][cell.y];
     if (walls_here.north == UNKNOWN || walls_here.east == UNKNOWN || walls_here.south == UNKNOWN || walls_here.west == UNKNOWN) {
@@ -210,13 +270,15 @@ class Maze {
     }
   }
 
+  /// @brief  return true if ALL the walls in a cell have been seen
   bool cell_is_visited(const Location cell) const {
     return not has_unknown_walls(cell);
   }
 
-  bool is_exit(const Location loc, const Heading heading) const {
+  /// @brief  Use the current mask to test if a given wall is an exit
+  bool is_exit(const Location cell, const Heading heading) const {
     bool result = false;
-    WallInfo walls = m_walls[loc.x][loc.y];
+    WallInfo walls = m_walls[cell.x][cell.y];
     switch (heading) {
       case NORTH:
         result = (walls.north & m_mask) == EXIT;
@@ -237,53 +299,27 @@ class Maze {
     return result;
   }
 
-  // Unconditionally set a wall state.
-  // Normally you only use this in setting up the maze at the start
-  void set_wall_state(const Location loc, const Heading heading, const WallState state) {
-    switch (heading) {
-      case NORTH:
-        m_walls[loc.x][loc.y].north = state;
-        m_walls[loc.north().x][loc.north().y].south = state;
-        break;
-      case EAST:
-        m_walls[loc.x][loc.y].east = state;
-        m_walls[loc.east().x][loc.east().y].west = state;
-        break;
-      case WEST:
-        m_walls[loc.x][loc.y].west = state;
-        m_walls[loc.west().x][loc.west().y].east = state;
-        break;
-      case SOUTH:
-        m_walls[loc.x][loc.y].south = state;
-        m_walls[loc.south().x][loc.south().y].north = state;
-        break;
-      default:
-        // ignore any other heading (blocked)
-        break;
-    }
-  }
-
-  // only change a wall if it is unknown
+  /// @brief only change a wall if it is unknown
   // This is what you use when exploring. Once seen, a wall should not be changed again.
-  void update_wall_state(const Location loc, const Heading heading, const WallState state) {
+  void update_wall_state(const Location cell, const Heading heading, const WallState state) {
     switch (heading) {
       case NORTH:
-        if ((m_walls[loc.x][loc.y].north & UNKNOWN) != UNKNOWN) {
+        if ((m_walls[cell.x][cell.y].north & UNKNOWN) != UNKNOWN) {
           return;
         }
         break;
       case EAST:
-        if ((m_walls[loc.x][loc.y].east & UNKNOWN) != UNKNOWN) {
+        if ((m_walls[cell.x][cell.y].east & UNKNOWN) != UNKNOWN) {
           return;
         }
         break;
       case WEST:
-        if ((m_walls[loc.x][loc.y].west & UNKNOWN) != UNKNOWN) {
+        if ((m_walls[cell.x][cell.y].west & UNKNOWN) != UNKNOWN) {
           return;
         }
         break;
       case SOUTH:
-        if ((m_walls[loc.x][loc.y].south & UNKNOWN) != UNKNOWN) {
+        if ((m_walls[cell.x][cell.y].south & UNKNOWN) != UNKNOWN) {
           return;
         }
         break;
@@ -291,13 +327,10 @@ class Maze {
         // ignore any other heading (blocked)
         break;
     }
-    set_wall_state(loc, heading, state);
+    set_wall_state(cell, heading, state);
   }
 
-  /***
-   * Initialise a maze and the costs with border walls and the start cell
-   *
-   */
+  /// @brief set empty maze with border walls and the start cell, zero costs
   void initialise() {
     for (int x = 0; x < MAZE_WIDTH; x++) {
       for (int y = 0; y < MAZE_WIDTH; y++) {
@@ -332,10 +365,7 @@ class Maze {
     return m_mask;
   }
 
-  /***
-   * Assumes the maze has been flooded
-   */
-
+  /// @brief return cost for neighbour cell in supplied heading
   uint16_t neighbour_cost(const Location cell, const Heading heading) const {
     if (not is_exit(cell, heading)) {
       return MAX_COST;
@@ -344,11 +374,14 @@ class Maze {
     return m_cost[next_cell.x][next_cell.y];
   }
 
+  /// @brief  return the cost associated withthe supplied cell location
   uint16_t cost(const Location cell) const {
     return m_cost[cell.x][cell.y];
   }
 
   /***
+   * @brief basic manhattan flood of the maze
+   *
    * Very simple cell counting flood fills m_cost array with the
    * manhattan distance from every cell to the target.
    *
@@ -393,7 +426,7 @@ class Maze {
    *
    * This could be extended to look ahead then towards the goal but it
    * probably is not worth the effort
-   *
+   * @brief get the geating to the lowest cost neighbour
    * @param cell
    * @param start_heading
    * @return
@@ -433,8 +466,33 @@ class Maze {
   }
 
  private:
+  // Unconditionally set a wall state.
+  // use update_wall_state() when exploring
+  void set_wall_state(const Location loc, const Heading heading, const WallState state) {
+    switch (heading) {
+      case NORTH:
+        m_walls[loc.x][loc.y].north = state;
+        m_walls[loc.north().x][loc.north().y].south = state;
+        break;
+      case EAST:
+        m_walls[loc.x][loc.y].east = state;
+        m_walls[loc.east().x][loc.east().y].west = state;
+        break;
+      case WEST:
+        m_walls[loc.x][loc.y].west = state;
+        m_walls[loc.west().x][loc.west().y].east = state;
+        break;
+      case SOUTH:
+        m_walls[loc.x][loc.y].south = state;
+        m_walls[loc.south().x][loc.south().y].north = state;
+        break;
+      default:
+        // ignore any other heading (blocked)
+        break;
+    }
+  }
   MazeMask m_mask = MASK_OPEN;
-  Location m_goal_loc{7, 7};
+  Location m_goal{7, 7};
   // on Arduino only use 8 bits for cost to save space
   uint8_t m_cost[MAZE_WIDTH][MAZE_WIDTH];
   WallInfo m_walls[MAZE_WIDTH][MAZE_WIDTH];
