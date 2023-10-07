dcdd84836254a9242cc96215b71b696915e4857b
micromouseonline <peter.harrison@helicron.net>
2023-10-07 20:26:10 +0100

    select UK contest event

--- mazerunner-core/config.h
+++ mazerunner-core/config.h
@@ -86,7 +86,7 @@ struct TurnParameters {
 #define EVENT_APEC 4
 
 // choose the one you will be using BEFORE selecting the robot below
-#define EVENT EVENT_HOME
+#define EVENT EVENT_UK
 #if EVENT == EVENT_HOME
 #define GOAL Location(2, 2)
 #else
