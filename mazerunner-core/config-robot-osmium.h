733707aca343974ce61fa5d1cb9cc999cc2b5062
micromouseonline <peter.harrison@helicron.net>
2024-07-28 14:14:45 +0100

    update mouse radius after motor change

--- mazerunner-core/config-robot-osmium.h
+++ mazerunner-core/config-robot-osmium.h
@@ -171,7 +171,7 @@ const float WHEEL_DIAMETER = 32.00;
 // small amount. AFTER you have the wheel diameter and gear ratio calibrated,
 // have the mouse turn in place and adjust the MOUSE_RADIUS until these turns are
 // as accurate as you can get them
-const float MOUSE_RADIUS = 38.70;  // 39.50; // Adjust on test
+const float MOUSE_RADIUS = 39.30;  // 39.50; // Adjust on test
 
 // The robot is likely to have wheels of different diameters or motors of slightly
 // different characteristics and that must be compensated for if the robot is to
