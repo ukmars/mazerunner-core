462bb387db93aa95f300dc449d56030bc55997f5
Peter Harrison <peter.harrison@helicron.net>
2025-10-20 16:00:40 +0100

    better logging in random walk

--- mazerunner-core/mouse.h
+++ mazerunner-core/mouse.h
@@ -281,25 +281,28 @@ class Mouse {
         break;
       }
       Serial.println();
-      reporter.log_action_status('-', ' ', m_location, m_heading);
+      reporter.log_action_status('#', ' ', m_location, m_heading);
       sensors.set_steering_mode(STEER_NORMAL);
       m_location = m_location.neighbour(m_heading);
       update_map();
-      Serial.write(' ');
-      Serial.write('|');
-      Serial.write(' ');
+      // Serial.write(' ');
+      // Serial.write('|');
+      // Serial.write(' ');
       char action = '#';
       if (m_location != target) {
         if (!sensors.see_left_wall) {
+          // Serial.println();
           turn_left();
           action = 'L';
         } else if (!sensors.see_front_wall) {
           move_ahead();
           action = 'F';
         } else if (!sensors.see_right_wall) {
+          // Serial.println();
           turn_right();
           action = 'R';
         } else {
+          // Serial.println();
           turn_back();
           action = 'B';
         }
@@ -519,29 +522,23 @@ class Mouse {
         break;
       }
       Serial.println();
-      reporter.log_action_status('-', ' ', m_location, m_heading);
+      reporter.log_action_status('#', ' ', m_location, m_heading);
       sensors.set_steering_mode(STEER_NORMAL);
       m_location = m_location.neighbour(m_heading);
       update_map();
       Serial.write(' ');
       Serial.write('|');
       Serial.write(' ');
-      char action = 'W';
       uint8_t hdg = randomHeading();
       if (hdg == LEFT) {
         turn_left();
-        action = 'L';
       } else if (hdg == AHEAD) {
         move_ahead();
-        action = 'F';
       } else if (hdg == RIGHT) {
         turn_right();
-        action = 'R';
       } else {
         turn_back();
-        action = 'B';
       }
-      reporter.log_action_status(action, ' ', m_location, m_heading);
     }
     // we are entering the target cell so come to an orderly
     // halt in the middle of that cell
